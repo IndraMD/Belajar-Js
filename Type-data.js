8dfa25413a66a7bb39e77ce2e27a3db345337507
IndraMD <indramuhamardani@gmail.com>
2023-11-19 14:51:59 +0700

    belajar type data dan memanggil variabel

--- Type-data.js
+++ Type-data.js
@@ -1,2 +1,17 @@
-const boolean =true;
-const name="sopyan";
+const umur = 17;
+console.log("type data umur adalah:",typeof umur,umur);
+
+let namaBuah = "Pepaya";
+namaBuah = "Anggur"
+console.log("type data nama buah adalah:",typeof namaBuah,namaBuah);
+
+var hoax = true;
+hoax = false
+console.log("type data hoax adalah",typeof hoax,hoax);
+
+var array = [];
+array = "**"
+console.log("type data array adalah",typeof array,array);
+
+var varBaru = false;
+console.log("type data baru",typeof varBaru,varBaru);
