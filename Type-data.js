11e221f79f3929c6fe5213d3bbdeb65be26c3935
IndraMD <indramuhamardani@gmail.com>
2023-11-19 16:07:39 +0700

    Tambah variabel Objek

--- Type-data.js
+++ Type-data.js
@@ -15,3 +15,23 @@ console.log("type data array adalah",typeof array,array);
 
 var varBaru = false;
 console.log("type data baru",typeof varBaru,varBaru);
+
+var NamaObject = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    year: 1925,
+    genres: ['Fiction', 'Classic'],
+    details: {
+      pages: 180,
+      publisher: 'Charles Scribner\'s Sons'
+    },
+    isBestseller: true,
+};
+
+console.log(NamaObject.title); // Output: 'John'
+console.log(NamaObject.author); // Output: 30
+console.log(NamaObject.details.pages); // Output: 30
+console.log(NamaObject.details.publisher); // Output: 30
+console.log(NamaObject.isBestseller); // Output: 30
+
+
