50c3f332e7d1726a29ab6ff5516a8c601fd18015
IndraMD <indramuhamardani@gmail.com>
2023-11-24 21:42:29 +0700

    ConvertUsd

---
+++ ConvertUsd.js
@@ -0,0 +1,13 @@
+function UsdToIdr(Usd=1, Idr = 15000) {
+    return Usd*Idr
+}
+
+console.log(UsdToIdr(2,20000))
+
+
+function namePeop(param1, param2) {
+    console.log(param1, param2)
+}
+
+namePeop('sopua','jsjsjsjsj')
+namePeop('i','a')
